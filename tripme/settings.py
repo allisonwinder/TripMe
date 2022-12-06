c0717c84780bb1a0fc0539c2475e4a21af852349
allisonwinder <103597663+allisonwinder@users.noreply.github.com>
2022-12-06 13:52:43 -0700

    Add database

--- tripme/settings.py
+++ tripme/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 import os
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -78,14 +79,10 @@ WSGI_APPLICATION = 'tripme.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'travel',
-        'USER': 'postgres',
-        'PASSWORD': '613012ckms',
-        'HOST': 'localhost'
-    }
+    "default": dj_database_url.config(default=DATABASE_URL, conn_max_age=1800),
 }
 
 
